8564da074b71db620a80ae26bd5556c1250ffddf
Davi Miranda Gonçalves <blizzardavi@hotmail.com>
2018-07-29 14:05:59 -0300

    .

--- garotasmagicas/Assets/Scripts/Manager/LevelManager.cs
+++ garotasmagicas/Assets/Scripts/Manager/LevelManager.cs
@@ -7,9 +7,13 @@ public class LevelManager : MonoBehaviour {
     float counter = 0;
     float pauseTime = 6.5f;
     int matchTime = 60;
-
+    public GameObject contador;
     float time;
+    float numero = 5f;
 
+    public Sprite tres;
+    public Sprite dois;
+    public Sprite um;
 
     // Use this for initialization
     void Start () {
@@ -19,9 +23,13 @@ public class LevelManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		 time -= Time.deltaTime;              
-	}
+		 time -= Time.deltaTime;
+        numero -= Time.deltaTime;
+        Debug.Log(numero);
+       
 
+
+    }
 
     void PauseAndResume()
     {
@@ -35,12 +43,34 @@ public class LevelManager : MonoBehaviour {
         yield return new WaitForEndOfFrame();
         counter += Time.unscaledDeltaTime;
         if (counter < timePeriod)
+        {
             StartCoroutine(ResumeAfterNSeconds(pauseTime));
+
+            if (counter >= 6.2)
+            {
+                contador.GetComponent<SpriteRenderer>().sprite = um;
+            }
+
+            if (counter >= 4.7 && counter < 6.2)
+            {
+                contador.GetComponent<SpriteRenderer>().sprite = dois;
+
+            }
+
+            if (counter >= 3 && counter < 4.7)
+            {
+                contador.GetComponent<SpriteRenderer>().sprite = tres;
+
+            }
+        }
         else
         {
             Time.timeScale = 1;
             counter = 0;
+            Destroy(contador);
         }
+
+     
     }
 
 
